767df58e537be72a1d61cd9ced61b3fbc4943eee
Murat Bulut <murobey729@gmail.com>
2023-02-07 21:50:30 +0300

    Update sistem-kapat.js

--- src/commands/sistem-kapat.js
+++ src/commands/sistem-kapat.js
@@ -6,13 +6,15 @@ const {
   ButtonStyle,
   ActionRowBuilder,
   WebhookClient,
+  PermissionFlagsBits,
 } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const db = require("orio.db");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("sistem-kapat")
-    .setDescription("Deprem bilgi sistemini kapatırsınız!"),
+    .setDescription("Deprem bilgi sistemini kapatırsınız!")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
   // komutu geliştirmek istersen guide: https://discordjs.guide/slash-commands/advanced-creation.html
   run: async (client, interaction) => {
     if (!db.get(`serverWebhook_${interaction.guild.id}`)) {
